04f087bd31ffd2b8d81f5243a17be28ec4b76107
xela <manghia@poloagroindustriale.edu.it>
2023-03-02 23:46:07 +0100

    aggiunto metodo get modificabili

--- libreria_flutter/lib/pages/home_page.dart
+++ libreria_flutter/lib/pages/home_page.dart
@@ -1,7 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:libreria_flutter/pages/getTitoli.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -12,62 +11,45 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final utente = FirebaseAuth.instance.currentUser!;
-  List<String> idLibri = [];
 
-  Future getLibriId() async {
-    await FirebaseFirestore.instance
-        .collection("Libri")
-        .get()
-        .then((snapshot) => snapshot.docs.forEach((element) {
-              idLibri.add(element.reference.id);
-            }));
+
+
+  getModificabili() async
+  {
+    var listaLibri=<dynamic>[];
+    var modificabili=<int>[];
+    String login="pippo";
+
+    await FirebaseDatabase.instance.ref().child("libri").get().then((snapshot) {
+      if (snapshot.exists) {
+        listaLibri = snapshot.value as List<dynamic>;
+      }
+    });
+
+    listaLibri.asMap().forEach((key, value) {
+      if(value!=null)
+        {
+          if(value["proprietario"]==login)
+          {
+            modificabili.add(key);
+          }
+          else
+          {
+
+          }
+        }
+    });
+
+    print(modificabili);
+
   }
 
-  @override
-  void initState() {
-    getLibriId();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
+    getModificabili();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          utente.email!,
-          style: TextStyle(fontSize: 16),
-        ),
-        actions: [
-          GestureDetector(
-              onTap: (){FirebaseAuth.instance.signOut();},
-              child:Icon(Icons.logout)
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: FutureBuilder(
-                future: getLibriId(),
-                builder: (context, snapshot) {
-                  return ListView.builder(
-                      itemCount: idLibri.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: GetTitoli(
-                            idLibro: idLibri[index],
-                            id: "Titolo",
-                          ),
-                        );
-                      });
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: Text("pippo"),
     );
   }
 }
